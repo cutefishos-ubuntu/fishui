c5985b603d9edd35d84b75f88e511dc5dde99bc0
SCRACX <robertlupas09@gmail.com>
2023-04-05 21:28:43 +0300

    Create ro_RO.ts
    Created the translation file for the Romanian language.

---
+++ translations/ro_RO.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>TextField</name>
+    <message>
+        <location filename="../src/fish-style/TextField.qml" line="62"/>
+        <source>Cut</source>
+        <translation>Decupează</translation>
+    </message>
+    <message>
+        <location filename="../src/fish-style/TextField.qml" line="68"/>
+        <source>Copy</source>
+        <translation>Copiază</translation>
+    </message>
+    <message>
+        <location filename="../src/fish-style/TextField.qml" line="74"/>
+        <source>Paste</source>
+        <translation>Lipește</translation>
+    </message>
+    <message>
+        <location filename="../src/fish-style/TextField.qml" line="79"/>
+        <source>Select All</source>
+        <translation>Selectează Tot</translation>
+    </message>
+</context>
+</TS>
